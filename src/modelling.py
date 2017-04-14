6523e80a3c108e1937e0a1d62a082606bbf78c9f
Sergii Golovko <sergii.golovko@gmail.com>
2017-04-14 06:22:16 -0400

    Model utils (#10)
    * Add model utils.
    * Change parameters.
    * Replace norm by scaling

--- src/modelling.py
+++ src/modelling.py
@@ -24,93 +24,118 @@ from globals import CONFIG
 from feature_classification import get_features, get_feature_classes
 from pickle_utils import load_X
 from cv_utils import get_cv
+from model_utils import tune_parameters, fit_and_predict, cross_validation
+from model_utils import get_classifiers, get_param_grids
 
 # Global directories.
 BASE_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..')
 DATA_DIR = os.path.join(BASE_DIR, CONFIG['DATA_DIR'])
 OUTPUT_DIR = os.path.join(BASE_DIR, CONFIG['OUTPUT_DIR'])
 PICKLE_DIR = os.path.join(BASE_DIR, CONFIG['PICKLED_DIR'])
+PRED_DIR = os.path.join(OUTPUT_DIR, CONFIG['PRED_DIR'])
 
 # Global files.
 TRAIN_FILE = os.path.join(DATA_DIR, 'train.csv')
 TEST_FILE = os.path.join(DATA_DIR, 'test.csv')
-PRED_FILE = os.path.join(OUTPUT_DIR, 'pred.csv')
 
 # Number of rows to read from files.
 TEST_NROWS = CONFIG['TEST_NROWS']
 TRAIN_NROWS = CONFIG['TRAIN_NROWS']
+
+# Turn off/on parameters tuning and cross validation.
+TUNE_PARAMETERS = True
+DO_CROSS_VALIDATION = True
+
+
+# TODO: this needs to be moved to utils type of file.
+def save_predictions(preds, name):
+
+    if not os.path.exists(PRED_DIR):
+        os.makedirs(PRED_DIR)
+
+    path = os.path.join(PRED_DIR, name + '.csv')
+    preds.to_csv(path, index=False)
+
+
+def generate_predictions(estimators, names, par_grids, class_features,
+                         class_feature_names, test_ids, y_train, sparse=False):
+
+    preds = test_ids.copy()
+
+    # CV for parameter tuning. To speed up the process I am doing stratisfied
+    # 2 iteration CV when tuning the parameters.
+    cv1 = get_cv(y_train)
+
+    # CV for cross validation. Must be KFold to create metafeatures.
+    cv2 = get_cv(y_train, n_folds=5, type='kfold')
+
+    for (class_feature, features_name) in zip(class_features, class_feature_names):
+
+        logging.info('Reading data from files.')
+        X_train, X_test = load_X(class_feature, len(y_train), sparse=sparse)
+        logging.info('Shape of X_train: %s' % str(X_train.shape))
+        logging.info('Data was succesfully read')
+
+        for (estimator, par_grid, name) in zip(estimators, par_grids, names):
+            filename = '_'.join((name, features_name))
+
+            if TUNE_PARAMETERS:
+                logging.info('Doing parameter tuning for %s model' % name)
+                best_params, best_score = tune_parameters(estimator, name,
+                                                          par_grid, X_train,
+                                                          y_train, cv1)
+                estimator.set_params(**best_params)
+                logging.info('Finished parameter tuning for %s model' % name)
+
+            if DO_CROSS_VALIDATION:
+                logging.info('Doing cross validation for %s model' % name)
+                cross_validation(estimator, X_train, y_train, cv2,
+                                 filename=filename)
+                logging.info('Finished cross validation for %s model' % name)
+
+            logging.info('Fitting %s model' % name)
+            preds['is_duplicate'] = (
+                fit_and_predict(estimator, X_train, y_train, X_test))
+            save_predictions(preds, filename)
+            logging.info('Finished fitting %s model' % name)
 
 
 def modelling():
 
     logging.info('MODELLING')
 
-    class_features = [
-        ['common_words', 'grouping', 'tfidf', 'common_vocabulary_svd_tfidf',
-         'distance_tfidf', 'word2vec'],
-        ['common_words', 'grouping', 'tfidf', 'svd_tfidf', 'distance_tfidf',
-         'word2vec']]
-
-    pred_files = [
-        os.path.join(OUTPUT_DIR, 'pred_common.csv'),
-        os.path.join(OUTPUT_DIR, 'pred_regular.csv')
-    ]
-
-    preds = pd.read_csv(TEST_FILE,
-                        usecols=['test_id'],
-                        nrows=TEST_NROWS)
-    y_train = pd.read_csv(TRAIN_FILE,
-                          usecols=['is_duplicate'],
+    test_ids = pd.read_csv(TEST_FILE, usecols=['test_id'], nrows=TEST_NROWS)
+    y_train = pd.read_csv(TRAIN_FILE, usecols=['is_duplicate'],
                           nrows=TRAIN_NROWS)
     y_train = y_train['is_duplicate'].values
 
-    for (class_feature, pred_file) in zip(class_features, pred_files):
+    # Generating predictions for logistic regression.
+    names = ['LogisticRegression']
+    estimators = get_classifiers(names)
+    par_grids = get_param_grids(names)
+    lr_class_features = [
+        ['common_words', 'grouping', 'tfidf', 'distance_tfidf', 'word2vec',
+         'raw_tfidf_question1', 'raw_tfidf_question2'],
+        ['common_words', 'grouping', 'tfidf', 'distance_tfidf', 'word2vec',
+         'common_vocabulary_raw_tfidf_question1',
+         'common_vocabulary_raw_tfidf_question2']
+    ]
+    class_feature_names = ['regular', 'common']
+    generate_predictions(estimators, names, par_grids, lr_class_features,
+                         class_feature_names, test_ids, y_train, sparse=True)
 
-        logging.info('Reading data from files.')
-        X_train, X_test = load_X(class_feature, len(y_train), sparse=False)
-        logging.info('Data was succesfully read')
-
-        clf = XGBClassifier(base_score=0.5,
-                            colsample_bylevel=1,
-                            colsample_bytree=0.9,
-                            gamma=0.7,
-                            learning_rate=0.03,
-                            max_delta_step=0,
-                            max_depth=9,
-                            min_child_weight=9.0,
-                            missing=None,
-                            n_estimators=430,
-                            nthread=-1,
-                            objective='binary:logistic',
-                            reg_alpha=0,
-                            reg_lambda=1,
-                            scale_pos_weight=1,
-                            seed=2016,
-                            silent=True,
-                            subsample=0.9)
-
-        # uncomment for logistic regression
-        # lr = LogisticRegression(C=10)
-
-        cv = get_cv(y_train)
-        logging.info('Shape of X_train: %s' % str(X_train.shape))
-        logging.info('Doing Cross Validation')
-        scores = cross_val_score(estimator=clf,
-                                 X=X_train,
-                                 y=y_train,
-                                 scoring='log_loss',
-                                 cv=cv,
-                                 n_jobs=1)
-        logging.info(scores)
-        logging.info('Finished Cross Validation')
-
-        logging.info('Fitting the model')
-        clf.fit(X_train, y_train)
-        logging.info('Finished fitting the model')
-
-        logging.info('Generating model predictions')
-        preds['is_duplicate'] = clf.predict_proba(X_test)[:, 1]
-        preds.to_csv(pred_file, index=False)
+    # Generating predictions for XGBoost.
+    names = ['XGBClassifier']
+    estimators = get_classifiers(names)
+    par_grids = get_param_grids(names)
+    xgb_class_features = [
+        ['common_words', 'grouping', 'tfidf', 'distance_tfidf', 'word2vec',
+         'svd_tfidf'],
+        ['common_words', 'grouping', 'tfidf', 'distance_tfidf', 'word2vec',
+         'common_vocabulary_svd_tfidf']
+        ]
+    generate_predictions(estimators, names, par_grids, xgb_class_features,
+                         class_feature_names, test_ids, y_train)
 
     logging.info('FINISHED MODELLING.')
 
